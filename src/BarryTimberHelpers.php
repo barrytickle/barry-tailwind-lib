e0b2441d236d80445015db9907c1f0f6f8ea1b68
Barry Tickle <barry.tickle12@gmail.com>
2025-03-02 01:39:11 +0000

    Updated the twig naming

--- src/BarryTimberHelpers.php
+++ src/BarryTimberHelpers.php
@@ -4,29 +4,30 @@ namespace BarryTimberHelpers;
 
 require_once __DIR__ . '/functions.php';
 
-use Timber\Twig_Function;
+use Timber\Timber;
+use Twig\TwigFunction; // Use the correct namespace for Timber 2.x
 
 class BarryTimberHelpers {
     public static function init() {
-        // Delay the Timber function registration until Timber is loaded
+        // Delay the function registration until Timber is fully loaded
         add_action('after_setup_theme', [self::class, 'add_custom_twig_functions'], 15);
     }
 
     public static function add_custom_twig_functions() {
-        // Check if Timber is available before adding functions
+        // Check if Timber is available
         if (!class_exists('Timber\Timber')) {
             return;
         }
 
         add_filter('timber/twig', function ($twig) {
-            $twig->addFunction(new Twig_Function('has_class_name', 'has_class_name'));
-            $twig->addFunction(new Twig_Function('is_svg', 'is_svg'));
-            $twig->addFunction(new Twig_Function('add_class_to_svg', 'add_class_to_svg'));
-            $twig->addFunction(new Twig_Function('post_meta', 'post_meta'));
-            $twig->addFunction(new Twig_Function('list_acf_fields_by_post_id', 'list_acf_fields_by_post_id'));
-            $twig->addFunction(new Twig_Function('get_acf_block_by_post_id', 'get_acf_block_by_post_id'));
-            $twig->addFunction(new Twig_Function('get_featured_image_by_post_id', 'get_featured_image_by_post_id'));
-            $twig->addFunction(new Twig_Function('get_url_by_post_id', 'get_url_by_post_id'));
+            $twig->addFunction(new TwigFunction('has_class_name', 'has_class_name'));
+            $twig->addFunction(new TwigFunction('is_svg', 'is_svg'));
+            $twig->addFunction(new TwigFunction('add_class_to_svg', 'add_class_to_svg'));
+            $twig->addFunction(new TwigFunction('post_meta', 'post_meta'));
+            $twig->addFunction(new TwigFunction('list_acf_fields_by_post_id', 'list_acf_fields_by_post_id'));
+            $twig->addFunction(new TwigFunction('get_acf_block_by_post_id', 'get_acf_block_by_post_id'));
+            $twig->addFunction(new TwigFunction('get_featured_image_by_post_id', 'get_featured_image_by_post_id'));
+            $twig->addFunction(new TwigFunction('get_url_by_post_id', 'get_url_by_post_id'));
 
             return $twig;
         });
--- src/functions.php
+++ src/functions.php
@@ -4,7 +4,6 @@
 function register_my_menu() {
     register_nav_menu('header-menu', __('Header Menu'));
 }
-
 add_action('init', 'register_my_menu');
 
 function has_class_name($object, $class_name) {
